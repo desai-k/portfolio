4262d727628f73f20ff20567b44b03db60770c31
desai-k <ksdesai@ucsd.edu>
2026-04-24 21:57:24 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,6 +1,12 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
-const container = document.querySelector('.projects');
 
+// Update title with project count
+const title = document.querySelector('.projects-title');
+if (title) {
+  title.textContent = `Projects (${projects.length})`;
+}
+
+const container = document.querySelector('.projects');
 renderProjects(projects, container, 'h2');
