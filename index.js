f1891a1d1bdfa8fd967dbbe023a4f5eeb96896df
desai-k <ksdesai@ucsd.edu>
2026-04-24 21:11:11 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
+
+const projects = await fetchJSON('./lib/projects.json');
+const latestProjects = projects.slice(0, 3);
+
+const container = document.querySelector('.projects');
+
+renderProjects(latestProjects, container, 'h2');
