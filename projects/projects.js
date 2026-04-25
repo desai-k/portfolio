aabae9a6e2d9992f0caf589420a6522e12671163
desai-k <ksdesai@ucsd.edu>
2026-04-24 21:56:42 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,7 +1,6 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
-
 const container = document.querySelector('.projects');
 
 renderProjects(projects, container, 'h2');
