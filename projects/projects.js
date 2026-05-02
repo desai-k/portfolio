50d72560f05ae0c685a412911c429793fe5f7aac
desai-k <ksdesai@ucsd.edu>
2026-05-01 17:24:10 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -37,7 +37,7 @@ const colors = d3.scaleOrdinal(d3.schemeTableau10)
 
 function renderPie(projectsData) {
   const svg = d3.select('#projects-pie-plot');
-  svg.classed('has-selection', selectedYear !== null);
+  // svg.classed('has-selection', selectedYear !== null);
   const legend = d3.select('.legend');
 
   // clear
@@ -105,7 +105,7 @@ function update() {
   const filtered = getFilteredProjects();
 
   renderProjects(filtered, container, 'h2');
-  renderPie(filtered);
+  renderPie(projects);
 }
 
 // initial render
